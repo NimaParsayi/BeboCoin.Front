6d1fa0dbdd2ae6ecff5027fd188627b45b1f60ca
nimaparsayi <nimaprs.ps@outlook.com>
2024-04-14 22:31:47 +0330

    change base url

--- src/constants/configuration.js
+++ src/constants/configuration.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://54.38.229.34:5050";
+export const BASE_URL = "https://bebobot.runflare.run";
 export const BOT_LINK = "https://t.me/bebocoinbot";
 export const TON_WALLET_BOT_LINK = "https://t.me/bebecoin_wallet_bot";
 
