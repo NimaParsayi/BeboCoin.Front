99ce6f6373069c7f0b3031184b04ccceb812981e
nimaparsayi <nimaprs.ps@outlook.com>
2024-04-23 12:50:52 +0330

    add bot mining count

--- src/views/Game/Game.vue
+++ src/views/Game/Game.vue
@@ -300,7 +300,7 @@ export default {
     },
     submitMining() {
       requestPost("/game/click", {
-        count: this.countClick,
+        count: this.countClick + this.botMining,
       }).then((json) => {
         if (!json) return;
 
