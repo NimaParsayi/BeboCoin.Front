aeb584f6dfbada525b26c0b5bbbb1d09c401ff9d
nimaparsayi <nimaprs.ps@outlook.com>
2024-05-01 14:02:37 +0330

    another fix

--- src/constants/configuration.js
+++ src/constants/configuration.js
@@ -1,5 +1,5 @@
 export const BASE_URL = "https://bebobot.runflare.run";
 export const BOT_LINK = "https://t.me/bebocoinbot";
 export const TON_WALLET_BOT_LINK = "https://t.me/bebecoin_wallet_bot";
-export const TON_CONNECT_MANIFEST_URL = "https://bebobot.runflare.run/tonconnect-manifest.json";
+export const TON_CONNECT_MANIFEST_URL = "https://bebocoin.runflare.run/tonconnect-manifest.json";
 
