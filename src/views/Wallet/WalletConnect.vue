27006ef12a11f4010d8ae55baf181f721b83bff0
nimaparsayi <nimaprs.ps@outlook.com>
2024-05-20 14:58:25 +0330

    unstable changes

--- src/views/Wallet/WalletConnect.vue
+++ src/views/Wallet/WalletConnect.vue
@@ -1,113 +1,80 @@
 <template>
-    <div>
-        <button @click="sendTransaction">Connect And Send</button>
-    </div>
+    <section class="section_wallet py-4">
+        <div class="flex-center flex-column">
+            <h1 class="fs-large fw-bold text-color">Deposit</h1>
+        </div>
+        <div class="mt-4 bg-card box-shadow p-3 rounded-2">
+            <w3m-button />
+        </div>
+        <div class="mt-3 bg-card box-shadow p-3 rounded-2">
+            <div class="d-flex flex-column">
+                <span class="description-color d-flex align-items-center fs-small fw-bold">
+                    <WalletIcon width="16px" height="16px" />
+                    Deposit To Wallet
+                </span>
+                <div class="d-flex align-items-center justify-content-between">
+                    <button @click="sendReq()" class="wallet_button flex-center rounded-2 box-shadow p-2 text-color fs-medium fw-bold">Send Transaction</button>
+                    <!-- <div class="col-6">
+                        <input class="w-full text-color fs-large fw-bold mt-1" v-model.number="amount"
+                            @keypress="isNumber($event)" type="number" />
+                    </div>
+                    <div class="flex-center col-6">
+                        <div class="col-3 bg-card mr-1 flex-center rounded-2 box-shadow p-1">
+                            <img width="35px" src="@/assets/images/icon/ton.png" alt="" />
+                        </div>
+
+                        <button @click="sendTonTransaction()"
+                            class="col-10 wallet_button flex-center rounded-2 box-shadow p-2">
+                            <span class="text-color fs-medium fw-bold flex-center"> Deposit </span>
+                        </button>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 <script setup>
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "@walletconnect/qrcode-modal";
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
+import { parseEther } from 'viem'
+import { mainnet, bsc } from 'viem/chains'
+import { reconnect, sendTransaction } from '@wagmi/core'
+//import { useWeb3Modal } from '@web3modal/wagmi/vue'
 
-let accountsList, chainIdVar;
+const projectId = 'b14e381d22f2009c0e104ada02de5bbd'
 
-// Create a connector
-const connector = new WalletConnect({
-    bridge: "https://bridge.walletconnect.org", // Required
-    qrcodeModal: QRCodeModal,
-});
-
-// Check if connection is already established
-if (!connector.connected) {
-    // create new session
-    connector.createSession();
+const metadata = {
+    name: 'Web3Modal',
+    description: 'Web3Modal Example',
+    url: 'https://web3modal.com', // origin must match your domain & subdomain
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
-// Subscribe to connection events
-connector.on("connect", (error, payload) => {
-    if (error) {
-        throw error;
-    }
+const chains = [mainnet, bsc]
+const config = defaultWagmiConfig({
+    chains,
+    projectId,
+    metadata,
+    //...wagmiOptions // Optional - Override createConfig parameters
+})
 
-    // Get provided accounts and chainId
-    const { accounts, chainId } = payload.params[0];
-    accountsList = accounts;
-    chainIdVar = chainId;
-});
+reconnect(config)
+// 3. Create modal
+createWeb3Modal({
+    wagmiConfig: config,
+    projectId,
+    enableAnalytics: true, // Optional - defaults to your Cloud configuration
+    enableOnramp: true // Optional - false as default
+})
 
-connector.on("session_update", (error, payload) => {
-    if (error) {
-        throw error;
-    }
+const sendReq = async () => {
 
-    // Get updated accounts and chainId
-    const { accounts, chainId } = payload.params[0];
-    accountsList = accounts;
-    chainIdVar = chainId;
-});
-
-connector.on("disconnect", (error, payload) => {
-    if (error) {
-        throw error;
-    }
-
-    // Delete connector
-    const { accounts, chainId } = payload.params[0];
-    accountsList = accounts;
-    chainIdVar = chainId;
-});
-
-const sendTransaction = () => {
-    console.log(accountsList);
-    console.log(chainIdVar);
-
-    const network = 118; // Atom (SLIP-44)
-    const account = accountsList.find((account) => account.network === network);
-    // Transaction structure based on Trust's protobuf messages.
-    const tx = {
-        accountNumber: "1035",
-        chainId: "cosmoshub-2",
-        fee: {
-            amounts: [
-                {
-                    denom: "uatom",
-                    amount: "5000",
-                },
-            ],
-            gas: "200000",
-        },
-        sequence: "40",
-        sendCoinsMessage: {
-            fromAddress: account.address,
-            toAddress: "cosmos1zcax8gmr0ayhw2lvg6wadfytgdhen25wrxunxa",
-            amounts: [
-                {
-                    denom: "uatom",
-                    amount: "100000",
-                },
-            ],
-        },
-    };
-
-    const request = connector._formatRequest({
-        method: "trust_signTransaction",
-        params: [
-            {
-                network,
-                transaction: JSON.stringify(tx),
-            },
-        ],
-    });
-
-    connector
-        ._sendCallRequest(request)
-        .then((result) => {
-            // Returns transaction signed in json or encoded format
-            console.log(result);
-        })
-        .catch((error) => {
-            // Error returned when rejected
-            console.error(error);
-        });
+    const result = await sendTransaction(config, {
+        to: '0xd2135CfB216b74109775236E36d4b433F1DF507B',
+        value: parseEther('0.01'),
+    })
+    console.log(result);
 }
+
 </script>
 <script>
 export default {
