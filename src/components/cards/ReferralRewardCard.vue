6a8cc3cdcd98a0b4f0308f30c55bbec0c18dc3c2
nimaparsayi <nimaprs.ps@outlook.com>
2024-06-12 16:49:38 +0330

    Minor Changes in Task

--- src/components/cards/ReferralRewardCard.vue
+++ src/components/cards/ReferralRewardCard.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="flex-center w-full flex-column box-shadow p-3 my-4 bg-card rounded-1" v-if="user.chatId === 1066096433">
-            <div class="d-flex flex-column w-full">
-                <span class="text-color fs-medium fw-bold">Invite Friends</span>
-                <div class="line my-2"></div>
-                <div class="flex-center flex-column w-full">
-                    <span>Invite 3 of your friends and get $1.</span>
-                    <p>You now invited {{ referralCount }}/3</p>
-                    <button class="box-shadow p-2 rounded-1 group_detail_join_button w-full">Get Reward</button>
-                </div>
+        <div class="d-flex flex-column w-full">
+            <span class="text-color fs-medium fw-bold">Invite Friends</span>
+            <div class="line my-2"></div>
+            <div class="flex-center flex-column w-full">
+                <span class="p-1">Invite 3 of your friends and get $1.</span>
+                <p class="p-1">You now invited {{ referralCount }}/3</p>
+                <button class="box-shadow p-2 m-1 rounded-1 group_detail_join_button w-full" @click="getReward">Get
+                    Reward</button>
             </div>
         </div>
+    </div>
 </template>
 <script>
 import { IsAuthenticated, requestGet, requestPost } from '@/stores/commonStore';
@@ -19,7 +20,7 @@ export default {
         return {
             user: null,
             isUserGotReferralReward: false,
-            referralCount : 0
+            referralCount: 0
         }
     },
     created() {
@@ -37,9 +38,21 @@ export default {
                 this.referralCount = json.result;
             });
         }
+    },
+    methods: {
+        getReward() {
+            requestPost('/Account/GetReferralReward').then((json) => {
+                if (json.status) {
+                    window.Swal.fire(`Task successfully completed.`, "", "ok");
+                    this.isUserGotReferralReward = true;
+                }
+            }).catch((err) => {
+                window.Swal.fire(err.result, "", "error");
+            });
+        }
     }
 }
 </script>
 <style lang="">
-    
+
 </style>
